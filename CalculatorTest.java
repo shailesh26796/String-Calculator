b49cb3c8124e60d59e8e60fddfd6fa79666653fd
Shailesh Pawar <82136436+shailesh26796@users.noreply.github.com>
2021-08-06 12:35:34 +0530

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -8,3 +8,7 @@ public class CalculatorTest{
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("stringcalculator.CalculatorTest");
     }
+@Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.add(""));
+	}
