eedbdb0c4cfca98d723baf36844298dac855e1df
Shailesh Pawar <82136436+shailesh26796@users.noreply.github.com>
2021-08-06 12:37:24 +0530

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -24,3 +24,7 @@ public class CalculatorTest{
     public void testThreeNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+ @Test
+    public void testNewLine(){
+    	assertEquals(6, Calculator.add("1\n2,3"));
+    }
