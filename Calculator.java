19cd5272ac3d0972fc0d4052bf1056c8214d4f22
Shailesh Pawar <82136436+shailesh26796@users.noreply.github.com>
2021-08-06 12:30:09 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -22,5 +22,26 @@ public class Calculator {
         private static String[] splitNumbers(String numbers, String divider){
 	    return numbers.split(divider);
 	}
+        private static int sum(String[] numbers){
+ 	    int total = 0;
+ 	    String negativeString = "";
 
+        for(String number : numbers){
+        	if(toInt(number) < 0){
+        		if(negativeString.equals(""))
+        			negativeString = number;
+        		else
+        			negativeString += ("," + number);
+        	}
+        	if(toInt(number) < 1000)
+		    	total += toInt(number);
+		}
+
+		if(!negativeString.equals("")){
+			throw new IllegalArgumentException("Negatives not allowed: " + negativeString);
+		}
+
+		return total;
+    }
+}
 
